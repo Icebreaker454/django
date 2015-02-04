4dc3602306dc93c95a9c483b3776891635ff92da
Paul Pukach <pavlopukach@gmail.com>
2015-02-04 13:40:57 +0200

    Added most of the views and urls, modified the index and groups pages

--- students/views.py
+++ students/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+#def students_list(request):
+#	template = loader.get_template('index.html')
+#	context = RequestContext(request, {})
+#	return HttpResponse(template.render(context))
+#EXACTLY THE SAME!
+
+# Views for students
+
+def students_list(request):
+	return render(request, 'students/index.html', {})
+def students_add(request):
+	return HttpResponse('<h1>Student add form</h1>')
+def students_edit(request, sid):
+	return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+def students_delete(request, sid):
+	return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
+#views for groups
+
+def groups_list(request):
+	return render(request, 'students/group_view.html', {})
+def groups_add(request):
+	return HttpResponse('<h1>Group add form</h1>')
+def groups_edit(request, gid):
+	return HttpResponse('<h1>Edit Group %s </h1>' % gid)
+def groups_delete(request, gid):
+	return HttpResponse('<h1>Delete Group %s </h1>' % gid)
