c2dafbbb34dcd2b1c55e884595b0fd2b93dc4d07
Paul Pukach <pavlopukach@gmail.com>
2015-02-21 12:26:04 +0200

    Added views and templates for student add form

--- students/views/students.py
+++ students/views/students.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from ..models.student import Student
+from ..models.group import Group
 
 #def students_list(request):
 #	template = loader.get_template('index.html')
@@ -60,7 +62,49 @@ def list(request):
 		}
 	)
 def add(request):
-	return HttpResponse('<h1>Student add form</h1>')
+	# Checking whether the method is Post
+	if request.method == 'POST':
+		# Checking if add button was clicked 
+		if request.POST.get('add_button') is not None:
+
+			# TODO: Validate input from user
+			errors = {}		
+
+			# If there is no errors, we create a student from the user input
+			if not errors:
+				student = Student(
+					first_name = request.POST['first_name'],
+					last_name = request.POST['last_name'],
+					middle_name = request.POST['middle_name'],
+					birthday = request.POST['birthday'],
+					ticket = request.POST['ticket'],
+					photo = request.FILES['photo'],
+					student_group = Group.objects.get(pk = request.POST['student_group'])
+				)
+				# Saving the dtudent to database
+				student.save()
+				return HttpResponseRedirect(reverse('home'))
+
+			# If we do have errors, we render the form with errors and previous user input
+			else:
+				return render(request, 'students/students_add.html',
+					{
+						'groups_list': Group.objects.all().order_by('title'),
+						'errors': errors
+					}
+				)
+		# if the cancel button was pressed, redirect to home
+		elif request.POST.get('cancel_button') is not None:
+			return HttpResponseRedirect(reverse('home'))
+	# initial form render
+	else:
+		groups_list = Group.objects.all().order_by('title')
+
+		return render(request, "students/students_add.html", 
+			{
+				'groups_list': groups_list
+			}
+		)	
 def edit(request, sid):
 	return HttpResponse('<h1>Edit Student %s</h1>' % sid)
 def delete(request, sid):
